33abe973cb3ef8f23fb4475d1d83c8bc3b7e2ef2
Ayush Sinha <sinhaayush203@gmail.com>
2024-11-13 12:13:04 -0500

    Counting
    Looping through the threads with the numebrs

--- Kraken/src/Main.java
+++ Kraken/src/Main.java
@@ -12,6 +12,26 @@ public class Main{
 
         ExecutorService service = Executors.newFixedThreadPool(NUM_THREADS);
         List<Future<Long>> results = new ArrayList<>();
-        
+
+        int range = MAX_VALUE / NUM_THREADS;
+
+        for (int i = 0; i < NUM_THREADS; i++)
+        {
+            int lowerLimit = i * range + 1;
+            int upperLimit = (i + 1) * range;
+
+            Callable<Long> task = () ->
+            {
+                long partialCount = 0;
+                for (int j = lowerLimit; j <= upperLimit; j++)
+                {
+                    partialCount++;
+                }
+                return partialCount;
+            };
+
+            results.add(service.submit(task));
+        }
+
     }
 }
